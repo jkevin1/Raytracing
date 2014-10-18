227a227feaff7c7256d1f0fc1050d6876dc6e602
jkevin1 <kevin_j53@ymail.com>
2014-10-18 14:41:12 -0400

    Set up boost to work with project, added simple thread test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include "image.h"
 #include "color.h"
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
+
+void testThread(int n) {
+	printf("Test thread %d started\n", n);
+	boost::posix_time::seconds time(n);
+	boost::this_thread::sleep(time);
+	printf("Test thread %d complete\n", n);
+}
 
 //This currently just tests the image/color class and writing the image to file
 int main(int argc, char* argv[]) {
@@ -9,9 +18,17 @@ int main(int argc, char* argv[]) {
 	image.write("blank.png");
 
 	printf("generating image\n");
-  	for(unsigned y = 0; y < image.height; y++)
-  		for(unsigned x = 0; x < image.width; x++)
+  	for(int y = 0; y < image.height; y++)
+  		for(int x = 0; x < image.width; x++)
 			image.pixel(x, y) = Color(255 * !(x & y), x ^ y, x | y, (unsigned char)255);
+
+	printf("Number of threads %d\n", boost::thread::hardware_concurrency());
+
+	boost::thread thread1(testThread, 1);
+	boost::thread thread2(testThread, 2);
+
+	thread1.join();
+	thread2.join();
 
 	printf("writing final image\n");
 	image.write("test.png");
