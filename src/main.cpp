73a3ed7cf38b3b1f5ec8de0b80eff67d2fe85710
jkevin1 <kevin_j53@ymail.com>
2014-10-20 23:44:27 -0400

    Slightly more complex thread test, started SSE Vec3 class implementation but considering not doing SSE because it may not be optimal

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "image.h"
 #include "color.h"
+#include "vec3.h"
 #include <boost/thread.hpp>
 #include <boost/date_time.hpp>
+#include <vector>
 
 void testThread(int n) {
 	printf("Test thread %d started\n", n);
@@ -21,14 +23,19 @@ int main(int argc, char* argv[]) {
   	for(int y = 0; y < image.height; y++)
   		for(int x = 0; x < image.width; x++)
 			image.pixel(x, y) = Color(255 * !(x & y), x ^ y, x | y, (unsigned char)255);
+	
+	//Testing threads
+	int n = boost::thread::hardware_concurrency();
+	printf("Number of threads %d\n", n);
+	std::vector<boost::thread> threads(n);
+	for (unsigned i = 0; i < threads.size(); i++) threads[i] = boost::thread(testThread, i+1);
+	for (unsigned i = 0; i < threads.size(); i++) threads[i].join();
 
-	printf("Number of threads %d\n", boost::thread::hardware_concurrency());
-
-	boost::thread thread1(testThread, 1);
-	boost::thread thread2(testThread, 2);
-
-	thread1.join();
-	thread2.join();
+	//Testing vector functions
+	Vec3 vec1;
+	Vec3 vec2(1.0f, -1.0f, 2.5f);
+	printf("vec1: %f %f %f\n", vec1.x, vec1.y, vec1.z);
+	printf("vec2: %f %f %f\n", vec2.x, vec2.y, vec2.z);
 
 	printf("writing final image\n");
 	image.write("test.png");
