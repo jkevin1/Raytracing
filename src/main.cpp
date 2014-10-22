4698abac5b7ce748e82487cca3befea536bc1898
jkevin1 <kevin_j53@ymail.com>
2014-10-22 18:38:22 -0400

    Finished SSE vec3 implementation

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,18 @@
 #include <boost/date_time.hpp>
 #include <vector>
 
+void print(const Vec3& v) {
+	printf("<%f, %f, %f>", v.x, v.y, v.z);
+}
+
+void println(const Vec3& v) {
+	printf("<%f, %f, %f>\n", v.x, v.y, v.z);
+}
+
 void testThread(int n) {
 	printf("Test thread %d started\n", n);
 	boost::posix_time::seconds time(n);
-	boost::this_thread::sleep(time);
+	//boost::this_thread::sleep(time);
 	printf("Test thread %d complete\n", n);
 }
 
@@ -25,18 +33,19 @@ int main(int argc, char* argv[]) {
 			image.pixel(x, y) = Color(255 * !(x & y), x ^ y, x | y, (unsigned char)255);
 	
 	//Testing threads
-	int n = boost::thread::hardware_concurrency();
-	printf("Number of threads %d\n", n);
+	unsigned n = boost::thread::hardware_concurrency();
+	printf("Number of threads %u\n", n);
 	std::vector<boost::thread> threads(n);
 	for (unsigned i = 0; i < threads.size(); i++) threads[i] = boost::thread(testThread, i+1);
 	for (unsigned i = 0; i < threads.size(); i++) threads[i].join();
 
 	//Testing vector functions
-	Vec3 vec1;
-	Vec3 vec2(1.0f, -1.0f, 2.5f);
-	printf("vec1: %f %f %f\n", vec1.x, vec1.y, vec1.z);
-	printf("vec2: %f %f %f\n", vec2.x, vec2.y, vec2.z);
-
+	Vec3 v1(3.0f, 0.0f, -1.0f);
+	Vec3 v2(1.0f, -1.0f, 2.5f);
+	printf("dot: %f\n", dot(v1, v2));
+	printf("angle: %f\n", angle(v1, v2));
+	printf("cross: "); println(cross(v1, v2));
+		
 	printf("writing final image\n");
 	image.write("test.png");
 }
